0bd5172eb68507134eb09e9548dc3f010eff1ff9
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2023-05-28 23:13:34 +0300

    Basics for recipes view

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe("Borch", "Ukrainian national soup", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXlhcbYxZCkR_Zf2BR-t37K1y17wqhKaK0cw&usqp=CAU")
+  ]
 }
